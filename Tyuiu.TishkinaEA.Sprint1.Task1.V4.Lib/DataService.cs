512c193c210b3f24ac6392a27dd7c5e2f6bee24f
tishkinaea@outlook.com <tishkinaea@outlook.com>
2024-11-08 13:33:06 +0300

    Task1

---
+++ Tyuiu.TishkinaEA.Sprint1.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TishkinaEA.Sprint1.Task1.V4.Lib
+{
+    public class DataService : ISprint1Task1V4
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x + y) / x * y;
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint1.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint1.Task1.V4.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
