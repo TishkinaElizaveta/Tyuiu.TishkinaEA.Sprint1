ac47200fefc38691a732306046f1b987e9e9a4a7
tishkinaea@outlook.com <tishkinaea@outlook.com>
2024-11-08 13:22:02 +0300

    Add project files.

---
+++ Tyuiu.TishkinaEA.Sprint1.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TishkinaEA.Sprint1.Task0.V29.Lib
+{
+    public class DataService : ISprint1Task0V29
+    {
+        public double Calculate()
+        {
+            return 2 * 9 + 7 * 2;
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint1.Task0.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TishkinaEA.Sprint1.Task0.V29.Lib;
+namespace Tyuiu.TishkinaEA.Sprint1.Task0.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Тишкина Елизавета Андреевна | РППБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Тишкина Елизавета Андреевна | РППБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
