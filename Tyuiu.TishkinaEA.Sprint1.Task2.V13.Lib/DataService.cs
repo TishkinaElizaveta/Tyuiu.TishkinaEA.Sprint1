2923bcb460feb56d00d53f46789379fd4f0610fd
admin <admin@DESKTOP-LHFSSMF>
2024-12-13 19:39:26 +0500

    task2

---
+++ Tyuiu.TishkinaEA.Sprint1.Task2.V13.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TishkinaEA.Sprint1.Task2.V13.Lib
+{
+    public class DataService : ISprint1Task2V13
+    {
+        public double ConvertMilesToKm(int value)
+        {
+             const double MilesToKilometers = 1.609344;
+            double kilometers = value * MilesToKilometers;
+
+            return Math.Round(kilometers, 3);
+        }
+    }
+}
