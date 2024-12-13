3a72374edd62bbd5946e4d0ff28f7b19fa949549
admin <admin@DESKTOP-LHFSSMF>
2024-12-13 22:08:25 +0500

    Task5

---
+++ Tyuiu.TishkinaEA.Sprint1.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TishkinaEA.Sprint1.Task5.V2.Lib
+{
+    public class DataService : ISprint1Task5V2
+    {
+        public int FahrenheitToСelsius(double temp)
+        {
+            double celsius = (temp - 32) * 5 / 9;
+            return Convert.ToInt32(celsius);
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint1.Task5.V2.Test/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint1.Task5.V2.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
