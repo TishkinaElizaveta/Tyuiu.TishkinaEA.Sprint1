2dc9d40f021a73e50c588ab1ad7fcb5b5b2d0763
admin <admin@DESKTOP-LHFSSMF>
2024-12-13 22:15:11 +0500

    Task6

---
+++ Tyuiu.TishkinaEA.Sprint1.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint1.Task6.V2.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint1.Task6.V2.Test/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint1.Task6.V2.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
